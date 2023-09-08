a84c91dd92ff7deac1d5a8264c4e9b69a8ce913d
Hiroaki Date <mmst3l4@gmail.com>
2023-09-09 05:17:11 +0900

    antdでレイアウトコンポーネントを作成

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import {AntdLayout} from "@/app/AntdLayout";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AntdLayout>
+          {children}
+        </AntdLayout>
+      </body>
     </html>
   )
 }
+
