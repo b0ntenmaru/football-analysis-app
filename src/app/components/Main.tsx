d219e64a01935bbcdf6c7a2660972ffd436c3188
Hiroaki Date <mmst3l4@gmail.com>
2023-09-16 00:55:42 +0900

    不要なファイルを削除してhooksを隔離

--- src/app/components/Main.tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from '@chakra-ui/react';
-
-export default function Main({ children }: { children: React.ReactNode }) {
-  return (
-    <Container as='main' maxW='container' my='4' minH='calc(100vh - 115px - 2rem)'>
-      {children}
-    </Container>
-  );
-}
