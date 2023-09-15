6f08f837a8510d7a5b420752d8caa1153e9fb3ba
Hiroaki Date <mmst3l4@gmail.com>
2023-09-16 01:07:44 +0900

    antdをlayoutに適用

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <div>hogehoge</div>;
-}
+'use client';
+
+import { Button, ConfigProvider } from 'antd';
+import React from 'react';
+import theme from '@/app/themeConfig';
+
+const HomePage = () => (
+  <ConfigProvider theme={theme}>
+    <Button type='primary' htmlType='submit'>
+      OK
+    </Button>
+    <Button style={{ marginLeft: 8 }}>Cancel</Button>
+  </ConfigProvider>
+);
+
+export default HomePage;
