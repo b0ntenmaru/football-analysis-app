c656b45b206f4158323a5b88d1b7f2fec0bf80e8
Hiroaki Date <d4te74@gmail.com>
2023-10-21 02:47:09 +0900

    既存のleaguesディレクトリをお掃除

--- src/app/leagues/[season_id]/SectionHeading.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const SectionHeading = ({ children }: { children: React.ReactNode }) => (
-  <div className='border-b border-gray-200 pb-5'>
-    <h3 className='text-base font-semibold leading-6 text-gray-900'>{children}</h3>
-  </div>
-);
