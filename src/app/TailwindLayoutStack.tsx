9d6e951527b0b639d3bcd2fec91b0a753f83f0bf
Hiroaki Date <mmst3l4@gmail.com>
2023-12-14 14:58:57 +0900

    [from now] 2023/12/14 14:58:57
    diff --git a/src/app/TailwindLayoutStack.tsx b/src/app/TailwindLayoutStack.tsx
    index fe54a19..2101a63 100644
    --- a/src/app/TailwindLayoutStack.tsx
    +++ b/src/app/TailwindLayoutStack.tsx
    @@ -198,7 +198,7 @@ export function TailwindLayoutStack({ children }: { children: React.ReactNode })
               </div>
             </header>
             <main>
    -          <div className='mx-auto max-w-7xl py-6 sm:px-6 lg:px-8'>{/* Your content */}</div>
    +          <div className='mx-auto max-w-7xl py-6 sm:px-6 lg:px-8'>{children}</div>
             </main>
           </div>
         </>

--- src/app/TailwindLayoutStack.tsx
+++ src/app/TailwindLayoutStack.tsx
@@ -198,7 +198,7 @@ export function TailwindLayoutStack({ children }: { children: React.ReactNode })
           </div>
         </header>
         <main>
-          <div className='mx-auto max-w-7xl py-6 sm:px-6 lg:px-8'>{/* Your content */}</div>
+          <div className='mx-auto max-w-7xl py-6 sm:px-6 lg:px-8'>{children}</div>
         </main>
       </div>
     </>
