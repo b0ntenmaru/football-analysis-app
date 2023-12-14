1425e53de34cde6559fb1f3fef14a0ca3db13891
Hiroaki Date <mmst3l4@gmail.com>
2023-12-14 15:08:42 +0900

    不要な記述を削除

--- src/app/TailwindLayoutStack.tsx
+++ src/app/TailwindLayoutStack.tsx
@@ -1,26 +1,11 @@
 'use client';
 
-import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
-import { Fragment } from 'react';
+import { Disclosure } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 
-const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
-  imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-};
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Reports', href: '#', current: false },
-];
-const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' },
+  { name: 'Home', href: '/', current: true },
+  { name: 'Junkie', href: '/analysis', current: false },
 ];
 
 function classNames(...classes: string[]) {
@@ -48,8 +33,8 @@ export function TailwindLayoutStack({ children }: { children: React.ReactNode })
                     <div className='flex-shrink-0'>
                       <img
                         className='h-8 w-8'
-                        src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500'
-                        alt='Your Company'
+                        src='https://avatars.githubusercontent.com/u/30609858?v=4'
+                        alt='FOOTBALL Junkie'
                       />
                     </div>
                     <div className='hidden md:block'>
@@ -70,56 +55,6 @@ export function TailwindLayoutStack({ children }: { children: React.ReactNode })
                           </a>
                         ))}
                       </div>
-                    </div>
-                  </div>
-                  <div className='hidden md:block'>
-                    <div className='ml-4 flex items-center md:ml-6'>
-                      <button
-                        type='button'
-                        className='relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
-                      >
-                        <span className='absolute -inset-1.5' />
-                        <span className='sr-only'>View notifications</span>
-                        <BellIcon className='h-6 w-6' aria-hidden='true' />
-                      </button>
-
-                      {/* Profile dropdown */}
-                      <Menu as='div' className='relative ml-3'>
-                        <div>
-                          <Menu.Button className='relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
-                            <span className='absolute -inset-1.5' />
-                            <span className='sr-only'>Open user menu</span>
-                            <img className='h-8 w-8 rounded-full' src={user.imageUrl} alt='' />
-                          </Menu.Button>
-                        </div>
-                        <Transition
-                          as={Fragment}
-                          enter='transition ease-out duration-100'
-                          enterFrom='transform opacity-0 scale-95'
-                          enterTo='transform opacity-100 scale-100'
-                          leave='transition ease-in duration-75'
-                          leaveFrom='transform opacity-100 scale-100'
-                          leaveTo='transform opacity-0 scale-95'
-                        >
-                          <Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-                            {userNavigation.map((item) => (
-                              <Menu.Item key={item.name}>
-                                {({ active }) => (
-                                  <a
-                                    href={item.href}
-                                    className={classNames(
-                                      active ? 'bg-gray-100' : '',
-                                      'block px-4 py-2 text-sm text-gray-700',
-                                    )}
-                                  >
-                                    {item.name}
-                                  </a>
-                                )}
-                              </Menu.Item>
-                            ))}
-                          </Menu.Items>
-                        </Transition>
-                      </Menu>
                     </div>
                   </div>
                   <div className='-mr-2 flex md:hidden'>
@@ -156,49 +91,18 @@ export function TailwindLayoutStack({ children }: { children: React.ReactNode })
                     </Disclosure.Button>
                   ))}
                 </div>
-                <div className='border-t border-gray-700 pb-3 pt-4'>
-                  <div className='flex items-center px-5'>
-                    <div className='flex-shrink-0'>
-                      <img className='h-10 w-10 rounded-full' src={user.imageUrl} alt='' />
-                    </div>
-                    <div className='ml-3'>
-                      <div className='text-base font-medium text-white'>{user.name}</div>
-                      <div className='text-sm font-medium text-gray-400'>{user.email}</div>
-                    </div>
-                    <button
-                      type='button'
-                      className='relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
-                    >
-                      <span className='absolute -inset-1.5' />
-                      <span className='sr-only'>View notifications</span>
-                      <BellIcon className='h-6 w-6' aria-hidden='true' />
-                    </button>
-                  </div>
-                  <div className='mt-3 space-y-1 px-2'>
-                    {userNavigation.map((item) => (
-                      <Disclosure.Button
-                        key={item.name}
-                        as='a'
-                        href={item.href}
-                        className='block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white'
-                      >
-                        {item.name}
-                      </Disclosure.Button>
-                    ))}
-                  </div>
-                </div>
               </Disclosure.Panel>
             </>
           )}
         </Disclosure>
 
-        <header className='bg-white shadow-sm'>
-          <div className='mx-auto max-w-7xl px-4 py-4 sm:px-6 lg:px-8'>
-            <h1 className='text-lg font-semibold leading-6 text-gray-900'>Dashboard</h1>
-          </div>
-        </header>
+        {/*<header className='bg-white shadow-sm'>*/}
+        {/*  <div className='mx-auto max-w-5xl px-4 py-4 sm:px-6 lg:px-8'>*/}
+        {/*    <h1 className='text-lg font-semibold leading-6 text-gray-900'>Dashboard</h1>*/}
+        {/*  </div>*/}
+        {/*</header>*/}
         <main>
-          <div className='mx-auto max-w-7xl py-6 sm:px-6 lg:px-8'>{children}</div>
+          <div className='mx-auto max-w-5xl px-4 py-4 sm:px-6 lg:px-8'>{children}</div>
         </main>
       </div>
     </>
